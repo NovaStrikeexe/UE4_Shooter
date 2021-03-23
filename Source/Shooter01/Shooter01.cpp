e8639eaef003ad5f57e67eb30175a67ed6768758
Sergey_Artemiev <sergArtmiev0024@yandex.ru>
2021-03-23 22:38:37 +0500

    first commit add Sourse

---
+++ Source/Shooter01/Shooter01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Shooter01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Shooter01, "Shooter01" );
